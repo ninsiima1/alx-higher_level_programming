c2f6d911f218b11edf284f03dd1b9d286b2426c3
ninsiima1 <nsylvia926@gmail.com>
2024-06-19 06:10:54 -0700

    if the first argument can be converted to an integer:

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,5 +5,5 @@ const args = process.argv[2];
 if (args === undefined || isNaN(parseInt(args))) {
   console.log('Not a number');
 } else {
-  console.log('My number ' + parseInt(args));
+  console.log('My number: ' + parseInt(args));
 }
